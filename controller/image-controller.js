3ea347812c2991551015136a44a3271559636a9e
Uday <udaikiran0070@gmail.com>
2023-10-11 12:29:59 +0530

    server

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream'
 import mongoose from 'mongoose'
 
-const url = 'http://localhost:8000'
+const url = 'https://blog-h1ujk4qbz-uday-kiran-rs-projects.vercel.app/'
 const conn = mongoose.connection;
 
 let gfs, gridfsBucket;
